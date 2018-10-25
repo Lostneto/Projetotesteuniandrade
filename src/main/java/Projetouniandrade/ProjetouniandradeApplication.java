99f16c2ca9e11feccab063b0dcddf216b31c966a
João Neto <l.joaont@gmail.com>
2018-10-25 01:30:45 -0300

    Projeto Criado

---
+++ src/main/java/Projetouniandrade/ProjetouniandradeApplication.java
@@ -0,0 +1,12 @@
+package Projetouniandrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetouniandradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetouniandradeApplication.class, args);
+	}
+}
---
+++ src/test/java/Projetouniandrade/ProjetouniandradeApplicationTests.java
@@ -0,0 +1,16 @@
+package Projetouniandrade;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetouniandradeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
