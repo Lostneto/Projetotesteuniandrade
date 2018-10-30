fc612bd3bf5edd9f888a0b3deb624b181f4d9764
João Neto <l.joaont@gmail.com>
2018-10-29 23:12:17 -0300

    instancias de categorias

--- src/main/java/Projetouniandrade/ProjetouniandradeApplication.java
+++ src/main/java/Projetouniandrade/ProjetouniandradeApplication.java
@@ -1,12 +1,34 @@
 package Projetouniandrade;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetouniandradeApplication {
+import Projetouniandrade.domain.Categoria;
+import Projetouniandrade.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetouniandradeApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetouniandradeApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
